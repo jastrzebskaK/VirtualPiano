2827c5990039d60481bcf0cab7c7f57fbc96b2fe
Karolina Jastrzebska <karolina.jastrzebska2709@gmail.com>
2021-03-27 13:37:13 +0100

    Update files

--- piano.js
+++ piano.js
@@ -1,27 +1,42 @@
 
- document.addEventListener('keydown', event => {
-    if (event.code === "KeyA" ) {
-     var myAudioA = new Audio("A.mp3");
-     myAudioA.play();
-    } else if (event.code === "KeyS") {
-     var myAudioS = new Audio("S.mp3");
-     myAudioS.play();
-   } else if (event.code === "KeyD") {
-     var myAudioD = new Audio("D.mp3");
-     myAudioD.play();
-   } else if (event.code === "KeyF") {
-     var myAudioF = new Audio("F.mp3");
-     myAudioF.play();
-   } else if (event.code === "KeyG") {
-     var myAudioG = new Audio("G.mp3");
-     myAudioG.play();
-   } else if (event.code ===  "KeyH") {
-     var myAudioH = new Audio("H.mp3");
-     myAudioH.play();
-   } else if (event.code === "KeyJ") {
-     var myAudioJ = new Audio("J.mp3");
-     myAudioJ.play();
-   } else {
+document.addEventListener('keydown', event => {
+  if (event.code === "KeyA") {
+    var myAudioA = new Audio("A.mp3");
+    myAudioA.play();
+  } else if (event.code === "KeyS") {
+    var myAudioS = new Audio("S.mp3");
+    myAudioS.play();
+  } else if (event.code === "KeyD") {
+    var myAudioD = new Audio("D.mp3");
+    myAudioD.play();
+  } else if (event.code === "KeyF") {
+    var myAudioF = new Audio("F.mp3");
+    myAudioF.play();
+  } else if (event.code === "KeyG") {
+    var myAudioG = new Audio("G.mp3");
+    myAudioG.play();
+  } else if (event.code === "KeyH") {
+    var myAudioH = new Audio("H.mp3");
+    myAudioH.play();
+  } else if (event.code === "KeyJ") {
+    var myAudioJ = new Audio("J.mp3");
+    myAudioJ.play();
+  } else if (event.code === "KeyW") {
+    var myAudioW = new Audio("W.mp3");
+    myAudioW.play();
+  } else if (event.code === "KeyE") {
+    var myAudioE = new Audio("E.mp3");
+    myAudioE.play();
+  } else if (event.code === "KeyT") {
+    var myAudioT = new Audio("T.mp3");
+    myAudioT.play();
+  } else if (event.code === "KeyY") {
+    var myAudioY = new Audio("Y.mp3");
+    myAudioY.play();
+  } else if (event.code === "KeyU") {
+    var myAudioU = new Audio("U.mp3");
+    myAudioU.play();
+  } else {
     console.log("Warning")
-   }})
-   
+  }
+})
